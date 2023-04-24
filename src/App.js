0084e4fcbe3ec4c99b82e126f1c6fe5fb0c562bd
genevill <evillacian@gmail.com>
2023-04-23 18:56:31 -0700

    Updates

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
             <a href="./project-link">Project Link</a>
             <TimerApp />
             <CalculatorApp />
-            <DrumApp />
+            s
         </section>
         <section class="center" id="coding-challenges">
           <h1>Coding Challenges</h1>
