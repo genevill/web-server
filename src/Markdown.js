6faa21f6d111c2cb0b68b8931108cb735ab451ea
genevill <evillacian@gmail.com>
2023-05-02 07:14:50 +0000

    Updates

---
+++ src/Markdown.js
@@ -0,0 +1,53 @@
+import './Markdown.css';
+import { marked } from 'marked';
+
+const defaultText = 
+      "# Hello\n" +
+      "## Hello\n" +
+      "There's also [links](https://www.freecodecamp.org)\n\n" + 
+      "> Block Quotes!\n\n" +
+      "`<div></div>`\n" +
+      "```\n" +
+      "// this is multi-line code:\n" +
+      "function anotherExample(firstLine, lastLine) {\n" +
+      "   if (firstLine == '```' && lastLine == '```') {\n" +
+      "      return multiLineCode;\n" +
+      "   }\n" +
+      "}\n" +
+      "```\n" +
+      "- And of course there are lists.\n\n" +
+      "![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)\n" +
+      "**Hello**";
+
+const defaultParsed = marked.parse(defaultText);
+
+const TextBox = props => {
+  return <textarea id="markdown-editor" name="markdown-editor" rows="4" onInput={LoadText}>{defaultText}</textarea>
+}
+
+const PreviewBox = props => {
+  return <p id="markdown-preview" onLoad={LoadText}></p>
+}
+
+function LoadText() {
+  document.getElementById("markdown-preview").innerHTML = marked.parse(document.getElementById("markdown-editor").value);
+}
+
+// They don't necessarily need to take props
+// This one also has an explicit return
+export const MarkdownApp = () => {
+  return(
+    <div id="markdown-parent">
+      <div class="markdown-box" id="markdown-box1">
+        <TextBox />
+      </div>
+      <div class="markdown-box" id="markdown-box2">
+        <PreviewBox />
+      </div>
+    </div>
+  );
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("markdown-preview").innerHTML = marked.parse(document.getElementById("markdown-editor").value)
+});
---
+++ src/Quote.js
@@ -0,0 +1,69 @@
+import './Quote.css';
+
+const quotes = [
+"You know, Hobbes, some days even my lucky rocketship underpants don\'t help.",
+"― Bill Watterson",
+"Even if we don't have the power to choose where we come from, we can still choose where we go from there.",
+"— Stephen Chbosky",
+"And now that you don't have to be perfect, you can be good.",
+"— John Steinbeck",
+"Outside the windows the day was bright: golden sunshine, blue sky, pleasant wind... I wanted to punch the happy day in the face, grab it by the hair, and beat it until it told me what the hell it was so happy about.",
+"― Ilona Andrews",
+"I haven't had a very good day. I think I might still be hungover and everyone's dead and my root beer's gone.",
+"― Holly Black"
+];
+
+var rand = 0;
+
+const Title = props => (
+  <h1 id="quote-author-text" className="title">{props.text}</h1>
+);
+
+// Doesn't have to be ES6 if you don't want
+const Copy = props => {
+  return <p id="text-text">{props.text}</p>
+    }
+
+function NewQuoteButton() {
+//rand = (Math.floor(Math.random() * 5) * 2)
+  return (<button class="quote-button" id="quote-new-quote" onClick = { () => { rand = (Math.floor(Math.random() * 5) * 2);
+  document.getElementById("text-text").innerHTML = quotes[rand];
+  document.getElementById("author-text").innerHTML = quotes[rand + 1];                                                                 } } >New quote</button>);
+}
+
+function Tweet() {
+  var url = "https://twitter.com/intent/tweet?text="
+  url += quotes[rand]
+  url += "  "
+  url += quotes[rand + 1]
+  return <a 
+     id="quote-tweet-quote"
+     class="twitter-share-button"
+     target="_blank"
+     href={url}
+     data-size="large"
+    />
+  }
+
+// They don't necessarily need to take props
+// This one also has an explicit return
+export const QuoteApp = () => {
+  return(
+    <div class="quote-box" id="quote-box">
+      <div id="quote-text">
+        <Copy text={quotes[rand]} />
+      </div>
+      <div id="quote-author">
+        <Title text="Author" text={quotes[rand + 1]} />
+      </div>
+      <div class="quote-row">
+        <div class="quote-center-align">
+          <NewQuoteButton />
+        </div>
+        <div class="twitter-align quote-center-align">
+          <Tweet />
+        </div>
+      </div>
+    </div>
+  );
+}
