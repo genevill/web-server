24aa49593bf0e52126fd034a04324ab2f50d337c
genevill <evillacian@gmail.com>
2023-05-03 23:40:45 -0700

    Updates

--- src/App.js
+++ src/App.js
@@ -64,13 +64,13 @@ function App() {
 
 document.addEventListener("DOMContentLoaded", function () {
     if (document.getElementById("markdown-preview") != null && document.getElementById("markdown-editor") != null) {
-        if (document.getElementById("markdown-preview") == null) {
-            console.log("markdown-preview is null");
-        }
-        if (document.getElementById("markdown-editor") == null) {
-            console.log("markdown-editor is null");
-        }
         LoadText();
+    }
+    if (document.getElementById("markdown-preview") == null) {
+        console.log("markdown-preview is null");
+    }
+    if (document.getElementById("markdown-editor") == null) {
+        console.log("markdown-editor is null");
     }
 });
 
--- src/Markdown.js
+++ src/Markdown.js
@@ -38,8 +38,8 @@ export function LoadText() {
 export const MarkdownApp = () => {
   return(
     <div id="markdown-parent">
-          <div class="markdown-box" id="markdown-box1">
-              <TextBox text={defaultText} />
+      <div class="markdown-box" id="markdown-box1">
+        <TextBox text={defaultText} />
       </div>
       <div class="markdown-box" id="markdown-box2">
         <PreviewBox />
