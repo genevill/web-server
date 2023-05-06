0c608fdfbeb17ed3c4172b80b609ec528620028e
genevill <evillacian@gmail.com>
2023-05-05 23:03:17 -0700

    Updates

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { DrumApp } from './DrumMachine.js';
 import { QuoteApp } from './Quote.js';
 import { MarkdownApp } from './Markdown.js';
 import { LoadText } from './Markdown.js';
+import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
 
 //----App----------------------------------------------------------------------
 
