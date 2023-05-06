b17449e58a1d3b8080c7edc22ac5df781dd5a8fa
genevill <evillacian@gmail.com>
2023-05-05 23:08:43 -0700

    Updates

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     <div class="main">
       <main id="main">
         <section class="center" id="about-me">
-          <h2>About Me</h2>
+          <h3>About Me</h3>
           <div class="about-me-sub">
             <div class="about-me-div unselectable">
               <img class="unselectable"  src="https://github.com/genevill/genevill/blob/main/profilepic.JPG?raw=true" />
@@ -36,23 +36,23 @@ function App() {
           </div>
         </section>
         <section class="center" id="projects">
-            <h2 >Projects</h2>
+            <h3 >Projects</h3>
             <p class="project-tile">Project Tile</p>
             <a href="./project-link">Project Link</a>
-            <h3>Pomodoro Timer App</h3>
+            <h4>Pomodoro Timer App</h4>
             <TimerApp />
-            <h3>Calculator App</h3>
+            <h4>Calculator App</h4>
             <CalculatorApp />
-            <h3>Drum App</h3>
+            <h4>Drum App</h4>
             <DrumApp />
-            <h3>Markdown App</h3>
+            <h4>Markdown App</h4>
             <MarkdownApp />
-            <h3>Quote App</h3>
+            <h4>Quote App</h4>
             <QuoteApp />
 
         </section>
         <section class="center" id="coding-challenges">
-          <h2>Coding Challenges</h2>
+          <h3>Coding Challenges</h3>
         </section>
       </main>
     </div>
