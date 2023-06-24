aeb76f0a243d4eba0946cedc3ab63e15a43401d9
genevill <evillacian@gmail.com>
2023-06-24 12:02:17 -0700

    Updates

--- src/Quote.js
+++ src/Quote.js
@@ -53,6 +53,7 @@ export const QuoteApp = () => {
         return (
             <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         );
+    }
 
     useEffect(TweetButton);
 
