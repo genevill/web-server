1fd5b8c7e0a0ef1853b93774b81fe0ee3213c180
genevill <evillacian@gmail.com>
2023-06-24 00:12:42 -0700

    Updates

--- src/Markdown.js
+++ src/Markdown.js
@@ -30,7 +30,7 @@ export const MarkdownApp = () => {
     }
 
     const PreviewBox = props => {
-        return (<p id="markdown-preview" onLoad={LoadText}>{marked.parse(defaultText)}</p>)
+        return (<p id="markdown-preview" onLoad={LoadText}>marked.parse(defaultText)</p>)
     }
 
     function LoadText() {
