f2ab70878c502d01defcc4f5b76801f7d28deda3
genevill <evillacian@gmail.com>
2023-06-24 01:27:20 -0700

    Updates

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ function App() {
                 <a id="profile-link" href="https://play.google.com/store/apps/dev?id=6083836623150592844" target="_blank">Google Developer Profile</a>
                 <a id="profile-link" href="https://github.com/genevill" target="_blank">GitHub Portfolio</a>
             </div>
-            <div class="switcher-center">
+            <div class="center">
             <div>
                 <AppButton text="Pomodoro Timer"></AppButton>
                 <AppButton text="Calculator"></AppButton>
@@ -85,8 +85,10 @@ function App() {
             </div>
 
         </section>
-        <section class="center switcher-center" id="projects">
-            <Switcher class="switcher-center" />
+        <section class="center " id="projects">
+            <div class="switcher-center">
+                <Switcher />
+            </div>
         </section>
         <section class="center" id="coding-challenges">
         </section>
