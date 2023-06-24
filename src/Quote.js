7b49f72058b995a103116f7dcf30a1b502d94d5f
genevill <evillacian@gmail.com>
2023-06-24 02:01:18 -0700

    Updates

--- src/Quote.js
+++ src/Quote.js
@@ -63,7 +63,7 @@ export const QuoteApp = () => {
             <NewQuoteButton />
         </div>
         <div class="twitter-align quote-center-align">
-            <Tweet />
+                    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         </div>
         </div>
     </div>
