69b4c78e4b6e0ff0641a612b823ac95f13464def
genevill <evillacian@gmail.com>
2023-06-23 23:15:38 -0700

    Updates

--- src/Timer.js
+++ src/Timer.js
@@ -59,23 +59,23 @@ export const TimerApp = () => {
 
     const Increase = props => {
         return (<button class="timer-button timer-unselectable" id={props.id} onClick={() => {
-            props.idValue == "break-length" ? breakLength++ : sessionLength++;
+            props.idValue == "timer-break-length" ? breakLength++ : sessionLength++;
             if (breakLength > 60) breakLength = 60; if (sessionLength > 60) sessionLength = 60;
-            document.getElementById(props.idValue).innerHTML = (props.idValue == "break-length" ? breakLength : sessionLength);
+            document.getElementById(props.idValue).innerHTML = (props.idValue == "timer-break-length" ? breakLength : sessionLength);
             currentSession = sessionLength * 60;
             currentBreak = breakLength * 60;
-            document.getElementById("time-left").innerHTML = SessionFormat();
+            document.getElementById("timer-time-left").innerHTML = SessionFormat();
         }}><img id="timer-buttonimg" src={upCaret} /></button>)
     }
 
     const Decrease = props => {
         return (<button class="timer-button timer-unselectable" id={props.id} onClick={() => {
-            props.idValue == "break-length" ? breakLength-- : sessionLength--;
+            props.idValue == "timer-break-length" ? breakLength-- : sessionLength--;
             if (breakLength < 1) breakLength = 1; if (sessionLength < 1) sessionLength = 1;
-            document.getElementById(props.idValue).innerHTML = (props.idValue == "break-length" ? breakLength : sessionLength);
+            document.getElementById(props.idValue).innerHTML = (props.idValue == "timer-break-length" ? breakLength : sessionLength);
             currentSession = sessionLength * 60;
             currentBreak = breakLength * 60;
-            document.getElementById("time-left").innerHTML = SessionFormat();
+            document.getElementById("timer-time-left").innerHTML = SessionFormat();
         }}><img id="timer-buttonimg" src={downCaret} /></button>)
     }
 
