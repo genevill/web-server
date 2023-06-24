8d48fe3e92013f28ff3e210dce7c8b979d62036d
genevill <evillacian@gmail.com>
2023-06-23 21:14:41 -0700

    Updates

--- src/Timer.js
+++ src/Timer.js
@@ -2,111 +2,112 @@ import './Timer.css';
 
 //---Timer--------------------------------------------------------------------------
 
-var breakLength = 5;
-var sessionLength = 25;
-var currentBreak = breakLength * 60;
-var currentSession = sessionLength * 60;
-var onSession = true;
-var myTimer;
-var upCaret = "https://storage.cloud.google.com/genevill/383167_caret_up_icon.png";
-var downCaret = "https://storage.cloud.google.com/genevill/383170_caret_down_icon.png";
-var refresh = "https://storage.cloud.google.com/genevill/383083_refresh_reload_icon.png";
-var playPause = "https://storage.cloud.google.com/genevill/play-pause.png";
+export const TimerApp = () => {
+    var breakLength = 5;
+    var sessionLength = 25;
+    var currentBreak = breakLength * 60;
+    var currentSession = sessionLength * 60;
+    var onSession = true;
+    var myTimer;
+    var upCaret = "https://storage.cloud.google.com/genevill/383167_caret_up_icon.png";
+    var downCaret = "https://storage.cloud.google.com/genevill/383170_caret_down_icon.png";
+    var refresh = "https://storage.cloud.google.com/genevill/383083_refresh_reload_icon.png";
+    var playPause = "https://storage.cloud.google.com/genevill/play-pause.png";
 
 
-const Timer = () => {
-    onSession == true ? currentSession-- : currentBreak--;
-    if (currentSession == -1) {
-        onSession = false;
-        document.getElementById("timer-timer-label").innerHTML = "Break";
-        currentSession = sessionLength * 60;
-        document.getElementById("timer-beep").play();
+    const Timer = () => {
+        onSession == true ? currentSession-- : currentBreak--;
+        if (currentSession == -1) {
+            onSession = false;
+            document.getElementById("timer-timer-label").innerHTML = "Break";
+            currentSession = sessionLength * 60;
+            document.getElementById("timer-beep").play();
+        }
+        if (currentBreak == -1) {
+            onSession = true;
+            document.getElementById("timer-timer-label").innerHTML = "Session";
+            currentBreak = breakLength * 60;
+            document.getElementById("timer-beep").play();
+        }
+        document.getElementById("timer-time-left").innerHTML = SessionFormat();
     }
-    if (currentBreak == -1) {
-        onSession = true;
-        document.getElementById("timer-timer-label").innerHTML = "Session";
-        currentBreak = breakLength * 60;
-        document.getElementById("timer-beep").play();
-    }
-    document.getElementById("timer-time-left").innerHTML = SessionFormat();
-}
 
-const PlayPause = () => {
-    if (myTimer == null) {
-        myTimer = setInterval(Timer, 1000);
-    } else {
+    const PlayPause = () => {
+        if (myTimer == null) {
+            myTimer = setInterval(Timer, 1000);
+        } else {
+            clearInterval(myTimer);
+            myTimer = null;
+        }
+    }
+
+    const Reset = () => {
         clearInterval(myTimer);
         myTimer = null;
-    }
-}
-
-const Reset = () => {
-    clearInterval(myTimer);
-    myTimer = null;
-    sessionLength = 25;
-    breakLength = 5;
-    currentSession = sessionLength * 60;
-    currentBreak = breakLength * 60;
-    onSession = true;
-    document.getElementById("timer-timer-label").innerHTML = "Session";
-    document.getElementById("timer-session-length").innerHTML = sessionLength;
-    document.getElementById("timer-break-length").innerHTML = breakLength;
-    document.getElementById("timer-time-left").innerHTML = SessionFormat();
-    document.getElementById("timer-beep").pause();
-    document.getElementById("timer-beep").currentTime = 0;
-}
-
-const Increase = props => {
-    return (<button class="timer-button timer-unselectable" id={props.id} onClick={() => {
-        props.idValue == "break-length" ? breakLength++ : sessionLength++;
-        if (breakLength > 60) breakLength = 60; if (sessionLength > 60) sessionLength = 60;
-        document.getElementById(props.idValue).innerHTML = (props.idValue == "break-length" ? breakLength : sessionLength);
+        sessionLength = 25;
+        breakLength = 5;
         currentSession = sessionLength * 60;
         currentBreak = breakLength * 60;
-        document.getElementById("time-left").innerHTML = SessionFormat();
-    }}><img id="timer-buttonimg" src={upCaret} /></button>)
-}
-
-const Decrease = props => {
-    return (<button class="timer-button timer-unselectable" id={props.id} onClick={() => {
-        props.idValue == "break-length" ? breakLength-- : sessionLength--;
-        if (breakLength < 1) breakLength = 1; if (sessionLength < 1) sessionLength = 1;
-        document.getElementById(props.idValue).innerHTML = (props.idValue == "break-length" ? breakLength : sessionLength);
-        currentSession = sessionLength * 60;
-        currentBreak = breakLength * 60;
-        document.getElementById("time-left").innerHTML = SessionFormat();
-    }}><img id="timer-buttonimg" src={downCaret} /></button>)
-}
-
-function SessionFormat() {
-    let currentString = "";
-    if (onSession) {
-        currentString = currentSession;
-    } else {
-        currentString = currentBreak;
+        onSession = true;
+        document.getElementById("timer-timer-label").innerHTML = "Session";
+        document.getElementById("timer-session-length").innerHTML = sessionLength;
+        document.getElementById("timer-break-length").innerHTML = breakLength;
+        document.getElementById("timer-time-left").innerHTML = SessionFormat();
+        document.getElementById("timer-beep").pause();
+        document.getElementById("timer-beep").currentTime = 0;
     }
-    return (
-        Math.floor(currentString / 60).toString().padStart(2, '0') + ":" + (currentString % 60).toString().padStart(2, '0')
-    )
-}
-const TimerButton = props => {
-    return (<button class="timer-button timer-unselectable" id={props.id} onClick={props.function}><img id="timer-buttonimg" src={props.image} /></button>)
-}
 
-const Text = props => {
-    return (<p class="timer-unselectable" id={props.id}>{props.text}</p>)
-}
+    const Increase = props => {
+        return (<button class="timer-button timer-unselectable" id={props.id} onClick={() => {
+            props.idValue == "break-length" ? breakLength++ : sessionLength++;
+            if (breakLength > 60) breakLength = 60; if (sessionLength > 60) sessionLength = 60;
+            document.getElementById(props.idValue).innerHTML = (props.idValue == "break-length" ? breakLength : sessionLength);
+            currentSession = sessionLength * 60;
+            currentBreak = breakLength * 60;
+            document.getElementById("time-left").innerHTML = SessionFormat();
+        }}><img id="timer-buttonimg" src={upCaret} /></button>)
+    }
 
-const SessionTimer = props => {
-    return (<p class="timer-unselectable" id={props.id}>{(currentSession / 60).toString().padStart(2, '0') + ":" + (currentSession % 60).toString().padStart(2, '0')}</p>)
-}
+    const Decrease = props => {
+        return (<button class="timer-button timer-unselectable" id={props.id} onClick={() => {
+            props.idValue == "break-length" ? breakLength-- : sessionLength--;
+            if (breakLength < 1) breakLength = 1; if (sessionLength < 1) sessionLength = 1;
+            document.getElementById(props.idValue).innerHTML = (props.idValue == "break-length" ? breakLength : sessionLength);
+            currentSession = sessionLength * 60;
+            currentBreak = breakLength * 60;
+            document.getElementById("time-left").innerHTML = SessionFormat();
+        }}><img id="timer-buttonimg" src={downCaret} /></button>)
+    }
 
-const AudioElement = props => {
-    return (<audio id="timer-beep">
-        <source src="https://storage.cloud.google.com/genevill/mixkit-clock-countdown-bleeps-916.wav" type="audio/wav" />
-    </audio>)
-}
-export const TimerApp = () => {
+    function SessionFormat() {
+        let currentString = "";
+        if (onSession) {
+            currentString = currentSession;
+        } else {
+            currentString = currentBreak;
+        }
+        return (
+            Math.floor(currentString / 60).toString().padStart(2, '0') + ":" + (currentString % 60).toString().padStart(2, '0')
+        )
+    }
+    const TimerButton = props => {
+        return (<button class="timer-button timer-unselectable" id={props.id} onClick={props.function}><img id="timer-buttonimg" src={props.image} /></button>)
+    }
+
+    const Text = props => {
+        return (<p class="timer-unselectable" id={props.id}>{props.text}</p>)
+    }
+
+    const SessionTimer = props => {
+        return (<p class="timer-unselectable" id={props.id}>{(currentSession / 60).toString().padStart(2, '0') + ":" + (currentSession % 60).toString().padStart(2, '0')}</p>)
+    }
+
+    const AudioElement = props => {
+        return (<audio id="timer-beep">
+            <source src="https://storage.cloud.google.com/genevill/mixkit-clock-countdown-bleeps-916.wav" type="audio/wav" />
+        </audio>)
+    }
+
     return (
         <div class="timer-outer-box">
             <div class="timer-box timer-grid-container">
