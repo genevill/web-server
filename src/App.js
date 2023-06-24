d7ff2c717737df613bfcfad6228e26a07a7a76a3
genevill <evillacian@gmail.com>
2023-06-24 01:12:15 -0700

    Updates

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ function App() {
         <section id="background">
         </section>
         <section class="center" id="about-me">
-            <div id="about-me">
+            <div id="about-me" class="navborders">
                 <a id="profile-link" href="https://www.freecodecamp.org/genevill" target="_blank">freeCodeCamp Portfolio</a>
                 <a id="profile-link" href="https://play.google.com/store/apps/dev?id=6083836623150592844" target="_blank">Google Developer Profile</a>
                 <a id="profile-link" href="https://github.com/genevill" target="_blank">GitHub Portfolio</a>
